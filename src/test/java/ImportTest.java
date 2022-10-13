3874c0f2b448804911a9d1ba38b4af52226d6f89
Timothy Hanley <t.hanley4@nuigalway.ie>
2022-10-13 14:45:52 +0100

    Import Tests Improved

--- src/test/java/ImportTest.java
+++ src/test/java/ImportTest.java
@@ -4,20 +4,59 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ImportTest
 {
-    Student student = new Student("Timothy", new LocalDate(2000, 12, 31), 19417576);
+    Course course = new Course("Electronic and Computer Engineering", new LocalDate(2019, 9, 5), new LocalDate(2024, 5, 31));
     Lecturer lecturer = new Lecturer("Michael", new LocalDate(1980, 1, 1));
+    Module module = new Module("Software Engineering 3", "CT417");
+    Student student = new Student("Timothy", new LocalDate(2000, 12, 31), 19417576);
 
     /** Part A Import Tests **/
 
     @Test
-    public void testPartAImportStudent()
+    public void testCourseImport()
     {
-        assertEquals(student.getUsername(), "Timothy21");
+        assertEquals(course.getName(), "Electronic and Computer Engineering");
+        assertEquals(course.getStartDate(), new LocalDate(2019, 9, 5));
+        assertEquals(course.getEndDate(), new LocalDate(2024, 5, 31));
+
+        assertTrue(course.getModules().isEmpty());
+        assertTrue(course.getStudents().isEmpty());
     }
 
     @Test
-    public void testPartAImportLecturer()
+    public void testLecturerImport()
     {
         assertEquals(lecturer.getUsername(), "Michael42");
+        assertEquals(lecturer.getName(), "Michael");
+        assertEquals(lecturer.getDateOfBirth(), new LocalDate(1980, 1, 1));
+        assertEquals(lecturer.getAge(), 42);
+
+        assertTrue(lecturer.getModules().isEmpty());
+    }
+
+    @Test
+    public void testModuleImport()
+    {
+        assertEquals(module.getName(), "Software Engineering 3");
+        assertEquals(module.getCode(), "CT417");
+
+        assertTrue(module.getStudents().isEmpty());
+        assertTrue(module.getCourses().isEmpty());
+        assertNull(module.getLecturer());
+
+        module.setLecturer(lecturer);
+        assertEquals(module.getLecturer(), lecturer);
+    }
+
+    @Test
+    public void testStudentImport()
+    {
+        assertEquals(student.getName(), "Timothy");
+        assertEquals(student.getDateOfBirth(), new LocalDate(2000, 12, 31));
+        assertEquals(student.getAge(), 21);
+        assertEquals(student.getId(), 19417576);
+        assertEquals(student.getUsername(), "Timothy21");
+
+        assertTrue(student.getCourses().isEmpty());
+        assertTrue(student.getModules().isEmpty());
     }
 }
