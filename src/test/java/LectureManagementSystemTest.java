bda175336557d4983c98ec145253e96e92d21391
Timothy Hanley <t.hanley4@nuigalway.ie>
2022-10-13 15:14:24 +0100

    Formatting Fixes

--- src/test/java/LectureManagementSystemTest.java
+++ src/test/java/LectureManagementSystemTest.java
@@ -15,48 +15,46 @@ public class LectureManagementSystemTest
     @Test
     public void testAddStudentToCourse()
     {
-        System.out.println("testAddStudentToCourse");
+        System.out.println("\ntestAddStudentToCourse");
         lectureManagementSystem.addStudentToCourse(student, course);
         assertTrue(course.getStudents().contains(student));
         assertTrue(student.getCourses().contains(course));
         System.out.println("Course: " + course + "; Students: " + course.getStudents());
         System.out.println("Student: " + student + "; Courses: " + student.getCourses());
-        System.out.println("\n");
     }
 
     @Test
     public void testRemoveStudentFromCourse()
     {
-        System.out.println("testRemoveStudentFromCourse");
+        System.out.println("\ntestRemoveStudentFromCourse");
         lectureManagementSystem.removeStudentFromCourse(student, course);
         assertFalse(course.getStudents().contains(student));
         assertFalse(student.getCourses().contains(course));
-        System.out.println("Course: " + course + ", Students: " + course.getStudents());
-        System.out.println("Student: " + student + ", Courses: " + student.getCourses());
-        System.out.println("\n");
+        System.out.println("Course: " + course + "; Students: " + course.getStudents());
+        System.out.println("Student: " + student + "; Courses: " + student.getCourses());
     }
 
     @Test
     public void testAddModuleToCourse()
     {
-        System.out.println("testAddModuleToCourse");
+        System.out.println("\ntestAddModuleToCourse");
         lectureManagementSystem.addModuleToCourse(module, course);
         assertTrue(course.getModules().contains(module));
         assertTrue(module.getCourses().contains(course));
-        System.out.println(course + "\nModules: " + course.getModules() + "\n");
-        System.out.println(module + "\nCourses: " + module.getCourses() + "\n");
+        System.out.println("Course: " + course + "; Modules: " + course.getModules());
+        System.out.println("Module: " + module + "; Courses: " + module.getCourses());
     }
 
 
     @Test
     public void testRemoveModuleFromCourse()
     {
-        System.out.println("testRemoveModuleFromCourse");
+        System.out.println("\ntestRemoveModuleFromCourse");
         lectureManagementSystem.removeModuleFromCourse(module, course);
         assertFalse(course.getModules().contains(module));
         assertFalse(module.getCourses().contains(course));
-        System.out.println(course + "\nModules: " + course.getModules() + "\n");
-        System.out.println(module + "\nCourses: " + module.getCourses() + "\n");
+        System.out.println("Course: " + course + "; Modules: " + course.getModules());
+        System.out.println("Module: " + module + "; Courses: " + module.getCourses());
     }
 
     /** Module Tests **/
@@ -64,45 +62,45 @@ public class LectureManagementSystemTest
     @Test
     public void testAddStudentToModule()
     {
-        System.out.println("testAddStudentToModule");
+        System.out.println("\ntestAddStudentToModule");
         lectureManagementSystem.addStudentToModule(student, module);
         assertTrue(student.getModules().contains(module));
         assertTrue(module.getStudents().contains(student));
-        System.out.println(module + "\nStudents: " + module.getStudents() + "\n");
-        System.out.println(student + "\nModules: " + student.getModules() + "\n");
+        System.out.println("Student: " + student + "; Modules: " + student.getModules());
+        System.out.println("Module: " + module + "; Students: " + module.getStudents());
     }
 
     @Test
     public void testRemoveStudentFromModule()
     {
-        System.out.println("testRemoveStudentFromModule");
+        System.out.println("\ntestRemoveStudentFromModule");
         lectureManagementSystem.removeStudentFromModule(student, module);
         assertFalse(student.getModules().contains(module));
         assertFalse(module.getStudents().contains(student));
-        System.out.println(module + "\nStudents: " + module.getStudents() + "\n");
-        System.out.println(student + "\nModules: " + student.getModules() + "\n");
+        System.out.println("Student: " + student + "; Modules: " + student.getModules());
+        System.out.println("Module: " + module + "; Students: " + module.getStudents());
     }
 
     @Test
     public void testAddLecturerToModule()
     {
-        System.out.println("testAddLecturerToModule");
+        System.out.println("\ntestAddLecturerToModule");
         lectureManagementSystem.addLecturerToModule(lecturer, module);
         assertEquals(lecturer, module.getLecturer());
         assertTrue(lecturer.getModules().contains(module));
-        System.out.println(module + "\nLecturer: " + module.getLecturer() + "\n");
-        System.out.println(lecturer + "\nModules: " + lecturer.getModules() + "\n");
+        System.out.println("Lecturer: " + lecturer + "; Modules: " + lecturer.getModules());
+        System.out.println("Module: " + module + "; Lecturer: " + module.getLecturer());
     }
 
     @Test
     public void testRemoveLecturerFromModule()
     {
-        System.out.println("testRemoveLecturerFromModule");
+        System.out.println("\ntestRemoveLecturerFromModule");
         lectureManagementSystem.removeLecturerFromModule(lecturer, module);
         assertNull(module.getLecturer());
         assertFalse(lecturer.getModules().contains(module));
-        System.out.println(module + "\nLecturer: " + module.getLecturer() + "\n");
-        System.out.println(lecturer + "\nModules: " + lecturer.getModules() + "\n");
+        System.out.println("Lecturer: " + lecturer + "; Modules: " + lecturer.getModules());
+        System.out.println("Module: " + module + "; Lecturer: " + module.getLecturer());
     }
 
     /** Lecturer Tests **/
@@ -110,23 +108,23 @@ public class LectureManagementSystemTest
     @Test
     public void testAddModuleToLecturer()
     {
-        System.out.println("testAddModuleToLecturer");
+        System.out.println("\ntestAddModuleToLecturer");
         lectureManagementSystem.addModuleToLecturer(module, lecturer);
         assertTrue(lecturer.getModules().contains(module));
         assertEquals(lecturer, module.getLecturer());
-        System.out.println(lecturer + "\nModules: " + lecturer.getModules() + "\n");
-        System.out.println(module + "\nLecturer: " + module.getLecturer() + "\n");
+        System.out.println("Lecturer: " + lecturer + "; Modules: " + lecturer.getModules());
+        System.out.println("Module: " + module + "; Lecturer: " + module.getLecturer());
     }
 
     @Test
     public void testRemoveModuleFromLecturer()
     {
-        System.out.println("testRemoveModuleFromLecturer");
+        System.out.println("\ntestRemoveModuleFromLecturer");
         lectureManagementSystem.removeModuleFromLecturer(module, lecturer);
         assertFalse(lecturer.getModules().contains(module));
         assertNull(module.getLecturer());
-        System.out.println(lecturer + "\nModules: " + lecturer.getModules() + "\n");
-        System.out.println(module + "\nLecturer: " + module.getLecturer() + "\n");
+        System.out.println("Lecturer: " + lecturer + "; Modules: " + lecturer.getModules());
+        System.out.println("Module: " + module + "; Lecturer: " + module.getLecturer());
     }
 
     /** Student Tests **/
@@ -134,44 +132,44 @@ public class LectureManagementSystemTest
     @Test
     public void testAddCourseToStudent()
     {
-        System.out.println("testAddCourseToStudent");
+        System.out.println("\ntestAddCourseToStudent");
         lectureManagementSystem.addCourseToStudent(course, student);
         assertTrue(student.getCourses().contains(course));
         assertTrue(course.getStudents().contains(student));
-        System.out.println(student + "\nCourses: " + student.getCourses() + "\n");
-        System.out.println(course + "\nStudents: " + course.getStudents() + "\n");
+        System.out.println("Student: " + student + "; Courses: " + student.getCourses());
+        System.out.println("Course: " + course + "; Students: " + course.getStudents());
     }
 
     @Test
     public void testRemoveCourseFromStudent()
     {
-        System.out.println("testRemoveCourseFromStudent");
+        System.out.println("\ntestRemoveCourseFromStudent");
         lectureManagementSystem.removeCourseFromStudent(course, student);
         assertFalse(student.getCourses().contains(course));
         assertFalse(course.getStudents().contains(student));
-        System.out.println(student + "\nCourses: " + student.getCourses() + "\n");
-        System.out.println(course + "\nStudents: " + course.getStudents() + "\n");
+        System.out.println("Student: " + student + "; Courses: " + student.getCourses());
+        System.out.println("Course: " + course + "; Students: " + course.getStudents());
     }
 
     @Test
     public void testAddModuleToStudent()
     {
-        System.out.println("testAddModuleToStudent");
+        System.out.println("\ntestAddModuleToStudent");
         lectureManagementSystem.addModuleToStudent(module, student);
         assertTrue(student.getModules().contains(module));
         assertTrue(module.getStudents().contains(student));
-        System.out.println(student + "\nModules: " + student.getModules() + "\n");
-        System.out.println(module + "\nStudents: " + module.getStudents() + "\n");
+        System.out.println("Student: " + student + "; Modules: " + student.getModules());
+        System.out.println("Module: " + module + "; Students: " + module.getStudents());
     }
 
     @Test
     public void testRemoveModuleFromStudent()
     {
-        System.out.println("testRemoveModuleFromStudent");
+        System.out.println("\ntestRemoveModuleFromStudent");
         lectureManagementSystem.removeModuleFromStudent(module, student);
         assertFalse(student.getModules().contains(module));
         assertFalse(module.getStudents().contains(student));
-        System.out.println(student + "\nModules: " + student.getModules() + "\n");
-        System.out.println(module + "\nStudents: " + module.getStudents() + "\n");
+        System.out.println("Student: " + student + "; Modules: " + student.getModules());
+        System.out.println("Module: " + module + "; Students: " + module.getStudents());
     }
 }
